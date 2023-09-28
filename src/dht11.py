b3d402bed3b40252e1fad9d60cd3151cd6b1789c
jpantonow <jpantonow@gmail.com>
2023-09-28 16:34:54 -0300

    alteracoes dht11

--- src/dht11.py
+++ src/dht11.py
@@ -7,9 +7,8 @@ import dht
 temperatura = 0
 humidade = 0
 #Declara onde esta conectado o sensor DHT22
-sensor = dht.DHT22(machine.Pin(15))
+sensor = dht.DHT22(machine.Pin(12))
 
-rele = machine.Pin(2, machine.Pin.OUT)
 
 def medir():
     #Fazer as medições
@@ -22,15 +21,4 @@ def medir():
     return temperatura, humidade
 
 
-
-
-#Loop infinito
-while True:
-    temperatura, humidade = medir()
-    print(f"A temperatura medida é: {temperatura} e a humidade é {humidade}")
-    time.sleep(1)
-    if temperatura > 25:
-        rele.value(1)
-    else:
-        rele.value(0)
 
