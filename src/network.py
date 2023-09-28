35c397fad233f1798094a587f752ee5b1e478042
jpantonow <jpantonow@gmail.com>
2023-09-28 19:42:54 -0300

    tirei o publish

--- src/network.py
+++ src/network.py
@@ -80,11 +80,6 @@ client.subscribe(hum_feed)
 
 client.publish(temp_feed,bytes(str(temp), 'utf-8'),qos=0)   # Publishing Temprature to adafruit.ioqos=0)
 client.publish(hum_feed,bytes(str(hum), 'utf-8'),qos=0)   # Publishing humity to adafruit.ioqos=0)
-client.publish(toggle_feed_1,bytes(str(ar_condicionado), 'utf-8'),qos=0)
-client.publish(toggle_feed_2,bytes(str(geladeira), 'utf-8'),qos=0)
-client.publish(toggle_feed_3,bytes(str(motor), 'utf-8'),qos=0)
-client.publish(toggle_feed_4,bytes(str(led_entrada), 'utf-8'),qos=0)
-client.publish(toggle_feed_5,bytes(str(led_sotao), 'utf-8'),qos=0)
 
         
 
