e546d7b6d5c4c05bf1e19412d8853994670d3b28
jpantonow <jpantonow@gmail.com>
2023-09-29 03:46:01 -0300

    quase final

--- src/main.py
+++ src/main.py
@@ -87,18 +87,16 @@ def cb(topic,msg):
             time.sleep(2)
     if topic == b"jpgomes/feeds/motor":
         if msg == b"0":
-            #if porta == 0:
             motor.move(90)
             sleep(1)
             porta = 1
         if msg == b"1":
-            #if porta == 1:
             motor.move(0)
             sleep(1)
             porta = 0
 
 
-sensor = dht.DHT11(Pin(15))                  # DHT11 Sensor on Pin 4 of ESP32
+sensor = dht.DHT22(Pin(15))                  # DHT11 Sensor on Pin 4 of ESP32
 
 led=Pin(2,Pin.OUT)                          # Onboard LED on Pin 2 of ESP32
 
@@ -155,17 +153,7 @@ toggle_feed_4 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, TOGGLE_FEED_ID
 toggle_feed_5 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, TOGGLE_FEED_ID_5), 'utf-8') # format - techiesms/feeds/motor
 mq135_feed = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, MQ135_FEED_ID), 'utf-8') # format - techiesms/feeds/temp
 
-client.set_callback(cb)
-# client.set_callback(rele1_cb)
-# client.set_callback(rele2_cb)               
-client.subscribe(toggle_feed_1) # Subscribing to particular topic
-client.subscribe(toggle_feed_2)
-client.subscribe(toggle_feed_3)
-client.subscribe(toggle_feed_4)
-client.subscribe(toggle_feed_5)
-client.subscribe(temp_feed)
-client.subscribe(hum_feed)
-client.subscribe(mq135_feed)
+
 
 client.publish(temp_feed,bytes("0", 'utf-8'),qos=0)   # Publishing Temprature to adafruit.ioqos=0)
 client.publish(hum_feed,bytes("0", 'utf-8'),qos=0)   # Publishing humity to adafruit.ioqos=0)
@@ -182,43 +170,32 @@ def sens_data(data):
     ppm = (int(calculate_ppm(adc_value,calibration_avg)))/100
 
     client.publish(mq135_feed,bytes(str(ppm), 'utf-8'), qos = 0)
-    client.publish(temp_feed,    
-                  bytes(str(temp), 'utf-8'),   # Publishing Temprature to adafruit.io
-                  qos=0)
+    client.publish(temp_feed, bytes(str(temp), 'utf-8'),qos=0)   # Publishing Temprature to adafruit.io
     
-    client.publish(hum_feed,    
-                  bytes(str(hum), 'utf-8'),   # Publishing Temprature to adafruit.io
-                  qos=0)
+    client.publish(hum_feed,bytes(str(hum), 'utf-8'),qos=0)   # Publishing Temprature to adafruit.io
     print("Temp - ", str(temp))
     print("Hum - " , str(hum))
     print("Ppm - ", str(ppm))
     print('Msg sent')
 
 # Função para calcular a concentração de CO2 (aproximada)
+client.set_callback(cb)
+client.subscribe(toggle_feed_1) # Subscribing to particular topic
+client.subscribe(toggle_feed_2)
+client.subscribe(toggle_feed_3)
+client.subscribe(toggle_feed_4)
+client.subscribe(toggle_feed_5)
+client.subscribe(temp_feed)
+client.subscribe(hum_feed)
+client.subscribe(mq135_feed)
 
 timer = Timer(0)
-timer.init(period=500, mode=Timer.PERIODIC, callback = sens_data)
-
+timer.init(period=10000, mode=Timer.PERIODIC, callback = sens_data)
 
 while True:
     try:
         client.check_msg()
-        # if button.value() == 0:  # Botão pressionado
-        #     if porta == 0: #Se a porta estiver fechada vamos abrir
-        #         print("Botão pressionado! Abrindo a porta...")
-        #         # Movendo o servo para 180 graus
-        #         motor.move(90)
-        #         sleep(1)
-        #         porta = 1 #Porta Aberta
-        #         continue
 
-        #     else:
-        #         print("Botão pressionado! Fechando a porta...")
-        #         # Movendo o servo para 0 graus
-        #         motor.move(0)
-        #         sleep(1)
-        #         porta = 0 #Porta Aberta
-        #         continue                  # non blocking function
     except :
         client.disconnect()
         sys.exit()
