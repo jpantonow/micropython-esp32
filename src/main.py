fe2e5b697d18971d34505a9a6f9b20d9c95d22c9
jpantonow <jpantonow@gmail.com>
2023-09-29 02:06:55 -0300

    tentando botar mq135

--- src/main.py
+++ src/main.py
@@ -122,6 +122,7 @@ TOGGLE_FEED_ID_4 = 'led_entrada'
 TOGGLE_FEED_ID_5 = 'led_sotao'
 TEMP_FEED_ID      = 'temp'
 HUM_FEED_ID      = 'hum'
+MQ135_FEED_ID = 'mq135'
 
 def connect_wifi(): #Função para conectar no WIFI
   print("Connecting to WiFi", end="")
@@ -155,6 +156,8 @@ toggle_feed_2 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, TOGGLE_FEED_ID
 toggle_feed_3 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, TOGGLE_FEED_ID_3), 'utf-8') # format - techiesms/feeds/motor
 toggle_feed_4 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, TOGGLE_FEED_ID_4), 'utf-8') # format - techiesms/feeds/motor
 toggle_feed_5 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, TOGGLE_FEED_ID_5), 'utf-8') # format - techiesms/feeds/motor
+mq135_feed = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, MQ135_FEED_ID), 'utf-8') # format - techiesms/feeds/temp
+
 client.set_callback(cb)
 # client.set_callback(rele1_cb)
 # client.set_callback(rele2_cb)               
@@ -165,10 +168,11 @@ client.subscribe(toggle_feed_4)
 client.subscribe(toggle_feed_5)
 client.subscribe(temp_feed)
 client.subscribe(hum_feed)
+client.subscribe(mq135_feed)
 
 client.publish(temp_feed,bytes("0", 'utf-8'),qos=0)   # Publishing Temprature to adafruit.ioqos=0)
 client.publish(hum_feed,bytes("0", 'utf-8'),qos=0)   # Publishing humity to adafruit.ioqos=0)
-
+client.publish(mq135_feed, bytes("0", 'utf-8'), qos=0)
 def sens_data(data):
     
     sensor.measure()                    # Measuring 
